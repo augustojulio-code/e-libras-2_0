fbb619349e4aa264ddcb8583ebca65529eb3597f
Julio <julio.mech@hotmail.com>
2023-08-22 09:00:41 -0300

    First commit

---
+++ apielibras/src/main/java/br/com/elibras/apielibras/ApielibrasApplication.java
@@ -0,0 +1,13 @@
+package br.com.elibras.apielibras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApielibrasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApielibrasApplication.class, args);
+	}
+
+}
---
+++ apielibras/src/test/java/br/com/elibras/apielibras/ApielibrasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.elibras.apielibras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApielibrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
