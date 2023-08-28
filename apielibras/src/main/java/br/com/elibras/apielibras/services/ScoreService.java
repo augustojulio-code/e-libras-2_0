5d0926b6972e06e8eac258fbf4bf4f32f8cf41b7
Julio <julio.mech@hotmail.com>
2023-08-28 13:23:56 -0300

    implement score service methods

--- apielibras/src/main/java/br/com/elibras/apielibras/services/ScoreService.java
+++ apielibras/src/main/java/br/com/elibras/apielibras/services/ScoreService.java
@@ -1,12 +1,38 @@
 package br.com.elibras.apielibras.services;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.elibras.apielibras.domain.Score;
 import br.com.elibras.apielibras.repository.ScoreRepository;
 
 @Service
 public class ScoreService {
     @Autowired
     private ScoreRepository repository;
+
+    public List<Score> findAll() {
+
+        return repository.findAll();
+    }
+
+    public Score findById(String id) {
+        Optional<Score> obj = repository.findById(id);
+
+        return obj.orElseThrow();
+    }
+
+    public Score insert(Score obj) {
+
+        return repository.insert(obj);
+    }
+
+    public void deleteById(String id) {
+        findById(id);
+
+        repository.deleteById(id);
+    }
 }
