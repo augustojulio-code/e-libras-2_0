f74889a10690f14f7644e9c183b9c52916378a94
Julio <julio.mech@hotmail.com>
2023-09-01 07:45:29 -0300

    implement score endpoints

--- apielibras/src/main/java/br/com/elibras/apielibras/controllers/ScoreController.java
+++ apielibras/src/main/java/br/com/elibras/apielibras/controllers/ScoreController.java
@@ -1,15 +1,64 @@
 package br.com.elibras.apielibras.controllers;
 
+import java.net.URI;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import br.com.elibras.apielibras.domain.Score;
+import br.com.elibras.apielibras.services.ScoreService;
 
 @RestController
 @RequestMapping("/score")
 public class ScoreController {
 
+    /*
+     * @GetMapping
+     * public String testes() {
+     * return "Olá imbecil";
+     * }
+     */
+    @Autowired
+    private ScoreService service;
+
     @GetMapping
-    public String testes() {
-        return "Olá imbecil";
+    public ResponseEntity<List<Score>> findAll() {
+        List<Score> list = service.findAll();
+
+        return ResponseEntity.ok().body(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Score> findById(@PathVariable String id) {
+        Score obj = service.findById(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping
+    public ResponseEntity<Score> insert(@RequestBody Score obj) {
+        obj = service.insert(obj);
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+
+        return ResponseEntity.created(uri).build();
+
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteById(@PathVariable String id) {
+        findById(id);
+        service.deleteById(id);
+
+        return ResponseEntity.noContent().build();
     }
 }
